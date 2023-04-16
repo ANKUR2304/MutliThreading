cb3fc3c6569a2ae3ade0b78317ae178494ec5bae
ANKUR2304 <jakharankur5@gmail.com>
2023-04-16 13:22:22 +0530

    Second way of creating a new thread

--- MultiThreading/src/main/java/com/example/multithreading/MultiThreading1.java
+++ MultiThreading/src/main/java/com/example/multithreading/MultiThreading1.java
@@ -2,25 +2,28 @@ package com.example.multithreading;
 
 public class MultiThreading1 {
     public static void main(String args[]){
+        System.out.println("this code is executed on thread : " + Thread.currentThread().getName());
+
         // create new thread
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                throw new RuntimeException("Intentional Exception");
-            }
-        });
+        NewThread thread1 = new NewThread();
 
         // set thread parameters, exception handlers
-        thread1.setName("thread1");
+        //thread1.setName("thread1");
         thread1.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
             @Override
             public void uncaughtException(Thread thread, Throwable throwable) {
                 System.out.println("A critical error happened in " + thread.getName() + " stating " + throwable.getMessage());
             }
-
         });
 
         // start thread1
         thread1.start();
+    }
+
+    public static class NewThread extends Thread{
+        @Override
+        public void run(){
+            System.out.println("this code is executed on thread : " + this.getName());
+        }
     }
 }
